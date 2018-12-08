d642a9a5f690d489803a3fdb43627eca30cc3c34
amirwel <amirwelber@gmail.com>
2018-12-08 17:04:49 +0200

    Update model.js

--- server/app/api/publisher/model.js
+++ server/app/api/publisher/model.js
@@ -22,7 +22,7 @@ const PublisherSchema = new Schema({
 PublisherSchema.virtual('campaigns', {
     ref: 'campaign',
     localField: '_id',
-    foreignField: 'publisher',
+    foreignField: 'publisherId',
     justOne: false
 })
 
