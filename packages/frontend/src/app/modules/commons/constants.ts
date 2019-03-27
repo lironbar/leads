f91ab0431f0401fe7717d90d22a6873768b8db2c
lironbar <lironbk@icloud.com>
2019-03-27 23:52:25 +0200

    frontend changes ready 2

--- packages/frontend/src/app/modules/commons/constants.ts
+++ packages/frontend/src/app/modules/commons/constants.ts
@@ -1,5 +1,7 @@
+import {environment} from '../../../environments/environment';
+
 export class Constants {
     // public static get BASE_URL(): string { return 'http://localhost:8080'; }
-    public static get BASE_URL(): string { return 'http://be.leads.com:8080'; }
+    public static get BASE_URL(): string { return environment.baseUrl; }
     // public static get BASE_URL(): string { return 'http://192.168.1.104:8080'; }
 }
