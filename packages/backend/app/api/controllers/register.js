23994778cbbac15b31067dedbf9bb0c4493acc04
lironbar <lironbk@icloud.com>
2019-03-28 01:41:16 +0200

    frontend changes ready 3

--- packages/backend/app/api/controllers/register.js
+++ packages/backend/app/api/controllers/register.js
@@ -4,6 +4,7 @@ module.exports.register = async (req, res) => {
     try {
         req.body.role = req.params.role;
         const user = await User.create(req.body);
+        req.session.user = user;
         res.status(200);
         res.json(user);
     } catch (err) {
