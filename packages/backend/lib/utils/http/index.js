847a0d250a1b47d8b913d19df9db3f68b6f3ad87
Amir Welber <amirwelber@gmail.com>
2019-06-25 01:01:27 +0300

    no message

--- packages/backend/lib/utils/http/index.js
+++ packages/backend/lib/utils/http/index.js
@@ -1,14 +1,16 @@
 const Url = require('url');
-const handlers = { http: require('http'), https: require('https') };
 
 module.exports = {
     request: (url, options, data) => {
         return new Promise((resolve, reject) => {
             let { protocol } = Url.parse(url);
-            protocol = (protocol ? protocol.replace(/[^a-zA-Z]/, '') : 'http');
-            const handler = handlers[protocol];
-            if (!handler) {
-                throw 'Unsupported protocol';
+            let handler;
+            if (protocol === 'http') {
+                handler = require('http');
+            } else if (protocol === 'https') {
+                handler = require('https');
+            } else {
+                throw new Error(`Unsupported protocol ${protocol}`);
             }
             const req = handler.request(url, options, resolve);
             req.on('error', reject);
