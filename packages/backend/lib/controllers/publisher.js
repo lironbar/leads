4fc4d02fa5ddf0952665ca76d306881dd020400b
Amir Welber <amirwelber@gmail.com>
2019-06-25 01:35:38 +0300

    publisher and affiliate creation

--- packages/backend/lib/controllers/publisher.js
+++ packages/backend/lib/controllers/publisher.js
@@ -1,6 +1,22 @@
 const User = require('../models/user');
 const Campaign = require('../models/campaign');
 
+module.exports.create = async (req, res) => {
+    try {
+        if (req.session.user.role !== 'ADMIN') {
+            return res.end(403);
+        }
+        
+        const id = await new User({ role: 'PUBLISHER', ...req.body }).save()._id;
+        const publisher = User.findOne({ _id: id }, { _id: 1, name: 1, email: 1, phone: 1, address: 1, phc: 1, contact: 1 }).populate('campaigns');
+        res.status(200);
+        res.json(publisher);
+    } catch (err) {
+        res.status(500);
+        res.send(err);
+    }
+};
+
 module.exports.find = async (req, res) => {
     try {
         const publishers = await User.find({ role: 'PUBLISHER' }, { _id: 1, name: 1, email: 1, phone: 1, address: 1, phc: 1, contact: 1 }).populate('campaigns');
